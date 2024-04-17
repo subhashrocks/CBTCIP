a981324233e95893c2a91efd27a508dfea8e09d8
subhashrocks <151207439+subhashrocks@users.noreply.github.com>
2024-04-17 22:15:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,127 +1,203 @@
-var today = new Date();
-var day = today.getDay();
-var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
-console.log("Today is : " + daylist[day] + ".");
-var dayy = document.getElementById("day");
-dayy.innerText = daylist[day];
-//////
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// Selectors
 
-const d = new Date();
-let month = months[d.getMonth()];
-///
-const p = new Date();
-var v = p.getDate();
-var today = document.getElementById('todaydata')
-today.innerText = v + " " + month
-console.log(v)
-////////
-const cityName = document.getElementById('cityName');
-const subbtn = document.getElementById('subbtn');
-const cityname = document.getElementById('cityname');
-const temp = document.getElementById('temp');
-const stat = document.getElementById('infowe');
-const humid = document.getElementById('humid');
-const infoo = document.getElementById('infoo');
-const tempinfo = document.getElementsByClassName('tempinfo');
-// const val = document.getElementsById('cityName').value;
+const toDoInput = document.querySelector('.todo-input');
+const toDoBtn = document.querySelector('.todo-btn');
+const toDoList = document.querySelector('.todo-list');
+const standardTheme = document.querySelector('.standard-theme');
+const lightTheme = document.querySelector('.light-theme');
+const darkerTheme = document.querySelector('.darker-theme');
 
-const info = async (event) => {
+
+// Event Listeners
+
+toDoBtn.addEventListener('click', addToDo);
+toDoList.addEventListener('click', deletecheck);
+document.addEventListener("DOMContentLoaded", getTodos);
+standardTheme.addEventListener('click', () => changeTheme('standard'));
+lightTheme.addEventListener('click', () => changeTheme('light'));
+darkerTheme.addEventListener('click', () => changeTheme('darker'));
+
+// Check if one theme has been set previously and apply it (or std theme if not found):
+let savedTheme = localStorage.getItem('savedTheme');
+savedTheme === null ?
+    changeTheme('standard')
+    : changeTheme(localStorage.getItem('savedTheme'));
+
+// Functions;
+function addToDo(event) {
+    // Prevents form from submitting / Prevents form from relaoding;
     event.preventDefault();
-    let cityval = cityName.value;
-    // tempinfo.style.display = "inline";
-    if (cityval === "") {
-        cityname.innerText = "Kindly enter the city name";
-    }
 
+    // toDo DIV;
+    const toDoDiv = document.createElement("div");
+    toDoDiv.classList.add('todo', `${savedTheme}-todo`);
+
+    // Create LI
+    const newToDo = document.createElement('li');
+    if (toDoInput.value === '') {
+            alert("You must write something!");
+        } 
     else {
-        try {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityval}&appid=9a8f36c988912f74ac36469e91c1c4cf&units=metric`;
-            const response2 = await fetch(url);
-            const data2 = await response2.json();
-            // console.log(response);
-            console.log(data2);
-            const arrData2 = [data2];
-            const icon2 = arrData2[0].weather[0].icon;
-            let urll2 = ` http://openweathermap.org/img/wn/${icon2}@2x.png`;
-            temp.innerText = arrData2[0].main.temp + "°C";
-            cityname.innerText = arrData2[0].name + "," + arrData2[0].sys.country;
-            st = arrData2[0].weather[0].main;
-            stat.innerHTML = `<img src= http://openweathermap.org/img/wn/${icon2}@2x.png>`
-            infoo.innerText = arrData2[0].weather[0].description;
-            humid.innerText = "Humidity:" + arrData2[0].main.humidity;
+        // newToDo.innerText = "hey";
+        newToDo.innerText = toDoInput.value;
+        newToDo.classList.add('todo-item');
+        toDoDiv.appendChild(newToDo);
 
-        }
-        catch {
-            cityname.innerText = "Kindly enter the correct city name";
-            cityName.innerText = "";
-            temp.innerText = "";
-            stat.innerText = "";
-            infoo.innerText = "";
-            tempinfo.innerText = "";
-            humid.innerText = "";
+        // Adding to local storage;
+        savelocal(toDoInput.value);
 
-            // setTimeout(function () {
-            //     window.location.reload();
-            // }, 4000);
-        }
+        // check btn;
+        const checked = document.createElement('button');
+        checked.innerHTML = '<i class="fas fa-check"></i>';
+        checked.classList.add('check-btn', `${savedTheme}-button`);
+        toDoDiv.appendChild(checked);
+        // delete btn;
+        const deleted = document.createElement('button');
+        deleted.innerHTML = '<i class="fas fa-trash"></i>';
+        deleted.classList.add('delete-btn', `${savedTheme}-button`);
+        toDoDiv.appendChild(deleted);
 
+        // Append to list;
+        toDoList.appendChild(toDoDiv);
+
+        // CLearing the input;
+        toDoInput.value = '';
     }
-}
-subbtn.addEventListener('click', info);
-//////////////////
-const locbt = document.getElementById('locbt');
-var x = document.getElementById('demo')
-function info2() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
 
-const showPosition = async (position) => {
-    const a = position.coords.latitude;
-    const b = position.coords.longitude;
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${a}&lon=${b}&appid=9a8f36c988912f74ac36469e91c1c4cf&units=metric`;
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.log(response);
-    console.log(data);
-    const arrData = [data];
-    const icon = arrData[0].weather[0].icon;
-    let urll = ` http://openweathermap.org/img/wn/${icon}@2x.png`;
-    temp.innerText = arrData[0].main.temp + "°C";
-    cityname.innerText = arrData[0].name + "," + arrData[0].sys.country;
-    st = arrData[0].weather[0].main;
-    stat.innerHTML = `<img src= http://openweathermap.org/img/wn/${icon}@2x.png>`
-    infoo.innerText = arrData[0].weather[0].description;
-    humid.innerText = "Humidity:" + arrData[0].main.humidity;
+}   
+
+
+function deletecheck(event){
+
+    // console.log(event.target);
+    const item = event.target;
+
+    // delete
+    if(item.classList[0] === 'delete-btn')
+    {
+        // item.parentElement.remove();
+        // animation
+        item.parentElement.classList.add("fall");
+
+        //removing local todos;
+        removeLocalTodos(item.parentElement);
+
+        item.parentElement.addEventListener('transitionend', function(){
+            item.parentElement.remove();
+        })
+    }
+
+    // check
+    if(item.classList[0] === 'check-btn')
+    {
+        item.parentElement.classList.toggle("completed");
+    }
+
+
 }
 
-locbt.addEventListener('click', info2);
 
-////////////////
-document.querySelector('.first-button').addEventListener('click', function () {
-
-    document.querySelector('.animated-icon1').classList.toggle('open');
-});
-
-
-
-
-cityName.addEventListener("keypress", function (event) {
-
-    if (event.key === "Enter") {
-
-        event.preventDefault();
-        document.getElementById("subbtn").click();
+// Saving to local storage:
+function savelocal(todo){
+    //Check: if item/s are there;
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
     }
-});
-// const reloadtButton = document.querySelector("#reload");
-// // Reload everything:
-// function reload() {
-//     reload = location.reload();
-// }
-// // Event listeners for reload
-// reloadButton.addEventListener("click", reload, false);
+    else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+
+
+function getTodos() {
+    //Check: if item/s are there;
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.forEach(function(todo) {
+        // toDo DIV;
+        const toDoDiv = document.createElement("div");
+        toDoDiv.classList.add("todo", `${savedTheme}-todo`);
+
+        // Create LI
+        const newToDo = document.createElement('li');
+        
+        newToDo.innerText = todo;
+        newToDo.classList.add('todo-item');
+        toDoDiv.appendChild(newToDo);
+
+        // check btn;
+        const checked = document.createElement('button');
+        checked.innerHTML = '<i class="fas fa-check"></i>';
+        checked.classList.add("check-btn", `${savedTheme}-button`);
+        toDoDiv.appendChild(checked);
+        // delete btn;
+        const deleted = document.createElement('button');
+        deleted.innerHTML = '<i class="fas fa-trash"></i>';
+        deleted.classList.add("delete-btn", `${savedTheme}-button`);
+        toDoDiv.appendChild(deleted);
+
+        // Append to list;
+        toDoList.appendChild(toDoDiv);
+    });
+}
+
+
+function removeLocalTodos(todo){
+    //Check: if item/s are there;
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    const todoIndex =  todos.indexOf(todo.children[0].innerText);
+    // console.log(todoIndex);
+    todos.splice(todoIndex, 1);
+    // console.log(todos);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// Change theme function:
+function changeTheme(color) {
+    localStorage.setItem('savedTheme', color);
+    savedTheme = localStorage.getItem('savedTheme');
+
+    document.body.className = color;
+    // Change blinking cursor for darker theme:
+    color === 'darker' ? 
+        document.getElementById('title').classList.add('darker-title')
+        : document.getElementById('title').classList.remove('darker-title');
+
+    document.querySelector('input').className = `${color}-input`;
+    // Change todo color without changing their status (completed or not):
+    document.querySelectorAll('.todo').forEach(todo => {
+        Array.from(todo.classList).some(item => item === 'completed') ? 
+            todo.className = `todo ${color}-todo completed`
+            : todo.className = `todo ${color}-todo`;
+    });
+    // Change buttons color according to their type (todo, check or delete):
+    document.querySelectorAll('button').forEach(button => {
+        Array.from(button.classList).some(item => {
+            if (item === 'check-btn') {
+              button.className = `check-btn ${color}-button`;  
+            } else if (item === 'delete-btn') {
+                button.className = `delete-btn ${color}-button`; 
+            } else if (item === 'todo-btn') {
+                button.className = `todo-btn ${color}-button`;
+            }
+        });
+    });
+}
